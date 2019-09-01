e3ce057dc30b7a46ce7423504e2c0854ac72e8ee
Efim Shliamin <shliamin@icloud.com>
2019-09-02 00:03:06 +0300

    created request in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_182435) do
+ActiveRecord::Schema.define(version: 2019_09_01_205023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_08_25_182435) do
     t.string "latitude"
     t.string "longitude"
     t.string "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "latitude"
+    t.string "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
