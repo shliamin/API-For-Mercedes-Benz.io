f952ced106534516b8a6f88c3edf3cf439782358
Efim Shliamin <shliamin@icloud.com>
2019-09-02 01:07:26 +0300

    @requests.save fixed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -25,7 +25,7 @@ class RequestsController < ApplicationController
   # POST /requests.json
   def create
     @request = Request.new(request_params)
-
+    @request.save
     # respond_to do |format|
     #   if @request.save
     #     format.html { redirect_to @request, notice: 'Request was successfully created.' }
