3f1ba60f25ca0f29fcef21faa846903311af6241
Efim <efimshliamin@gmail.com>
2024-06-18 01:35:21 +0200

    new client side

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ async function findMuseums() {
     var radius = document.getElementById('radius').value;
 
     // Convert address to latitude and longitude
-    var geoResponse = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${address}`);
-    if (geoResponse.data.length > 0) {
-        var lat = geoResponse.data[0].lat;
-        var lon = geoResponse.data[0].lon;
+    var geoResponse = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${address}`);
+    var geoData = await geoResponse.json();
+
+    if (geoData.length > 0) {
+        var lat = geoData[0].lat;
+        var lon = geoData[0].lon;
 
         // Center the map on the provided location
         map.setView([lat, lon], 13);
 
         // Fetch museums from your API
-        var apiResponse = await axios.get(`https://geocodingapi-7cc21820406a.herokuapp.com/museums?lat=${lat}&lon=${lon}&
+        var apiResponse = await fetch(`https://geocodingapi-7cc21820406a.herokuapp.com/museums?lat=${lat}&lon=${lon}&radius=${radius}`);
+        var museums = await apiResponse.json();
+
+        // Clear existing markers
+        map.eachLayer(function (layer) {
+            if (!!layer.toGeoJSON) {
+                map.removeLayer(layer);
+            }
+        });
+
+        // Add markers for each museum
+        museums.forEach(function (museum) {
+            var marker = L.marker([museum.lat, museum.lon]).addTo(map);
+            marker.bindPopup(`<b>${museum.name}</b><br>${museum.email ? 'Email: ' + museum.email + '<br>' : ''}${museum.phone ? 'Phone: ' + museum.phone + '<br>' : ''}${museum.website ? '<a href="' + museum.website + '" target="_blank">Website</a>' : ''}`);
+        });
+    } else {
+        alert('Address not found');
+    }
+}
